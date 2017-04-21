a263e6f1b4711d78f92ff27c2eb807e6015cee42
Martens Cedric <1544256@cegepmontpetit.ca>
2017-04-21 15:35:51 -0400

    Cleaned default project added hexameter library and edited build.gradle

--- LD38/desktop/src/com/martenscedric/desktop/DesktopLauncher.java
+++ LD38/desktop/src/com/martenscedric/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.martenscedric.LudumDare38;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1200;
+		config.height = 675;
+		config.resizable = false;
 		new LwjglApplication(new LudumDare38(), config);
 	}
 }
