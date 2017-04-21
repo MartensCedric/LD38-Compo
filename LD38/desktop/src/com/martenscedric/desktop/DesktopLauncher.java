b3f0664d786a78321ad2341660a8d31516d41786
Martens Cedric <1544256@cegepmontpetit.ca>
2017-04-21 15:09:20 -0400

    Added LibGDX template to git

---
+++ LD38/desktop/src/com/martenscedric/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.martenscedric.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.martenscedric.LudumDare38;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LudumDare38(), config);
+	}
+}
