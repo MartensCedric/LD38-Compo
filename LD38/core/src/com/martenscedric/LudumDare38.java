45a115de56dca2e94b76c4ab5a99eb8299451b05
MartensCedric <cedricmartens98@gmail.com>
2017-04-21 22:38:32 -0400

    Added grid color maybe modified ideas idk

--- LD38/core/src/com/martenscedric/LudumDare38.java
+++ LD38/core/src/com/martenscedric/LudumDare38.java
@@ -2,10 +2,16 @@ package com.martenscedric;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.EarClippingTriangulator;
+import com.badlogic.gdx.utils.ShortArray;
 import org.codetome.hexameter.core.api.*;
 import org.codetome.hexameter.core.api.Point;
 import rx.Observable;
@@ -14,14 +20,17 @@ import rx.functions.Action1;
 
 public class LudumDare38 extends ApplicationAdapter {
 	private SpriteBatch batch;
-	private HexagonalGrid grid;
+	private PolygonSpriteBatch polyBatch;
+	private HexagonalGrid<TileData> grid;
 	private ShapeRenderer shapeRenderer;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
 
-		HexagonalGridBuilder builder = new HexagonalGridBuilder()
+		batch = new SpriteBatch();
+		polyBatch = new PolygonSpriteBatch();
+
+		HexagonalGridBuilder<TileData> builder = new HexagonalGridBuilder<TileData>()
 				.setGridHeight(9)
 				.setGridWidth(9)
 				.setGridLayout(HexagonalGridLayout.HEXAGONAL)
@@ -29,6 +38,9 @@ public class LudumDare38 extends ApplicationAdapter {
 				.setRadius(Gdx.graphics.getWidth()/16);
 
 		grid = builder.build();
+		initHexData();
+		fillHexs();
+		initInput();
 		shapeRenderer = new ShapeRenderer();
 		shapeRenderer.setAutoShapeType(true);
 	}
@@ -37,33 +49,109 @@ public class LudumDare38 extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		polyBatch.begin();
+		Observable<Hexagon<TileData>> hexagons = grid.getHexagons();
+		hexagons.forEach(hex -> {
+			TileData data = hex.getSatelliteData().get();
+			data.getSprite().draw(polyBatch);
+		});
+		polyBatch.end();
 		batch.begin();
 
 		batch.end();
 		shapeRenderer.begin();
-
-		Observable<Hexagon> hexagons = grid.getHexagons();
-		hexagons.forEach(hexagon -> {
-            Point p0 = (Point) hexagon.getPoints().toArray()[0];
-            Point pLast = (Point) hexagon.getPoints().toArray()[hexagon.getPoints().size() - 1];
-            shapeRenderer.line((float)p0.getCoordinateX(), (float)p0.getCoordinateY(),
-                    (float)pLast.getCoordinateX(), (float)pLast.getCoordinateY(),
-					Color.BLACK, Color.BLACK);
-            for(int i = 1; i < hexagon.getPoints().size(); i++)
-            {
-                Point current = (Point)hexagon.getPoints().toArray()[i];
-                Point precedent = (Point)hexagon.getPoints().toArray()[i - 1];
-                shapeRenderer.line((float)current.getCoordinateX(), (float)current.getCoordinateY(),
-                        (float)precedent.getCoordinateX(), (float)precedent.getCoordinateY(),
-                        Color.BLACK, Color.BLACK);
-            }
-        });
+		renderHexs();
 		shapeRenderer.end();
 	}
-	
+
+	private void initInput()
+	{
+		Gdx.input.setInputProcessor(new InputProcessor() {
+			@Override
+			public boolean keyDown(int keycode) {
+				return false;
+			}
+
+			@Override
+			public boolean keyUp(int keycode) {
+				return false;
+			}
+
+			@Override
+			public boolean keyTyped(char character) {
+				return false;
+			}
+
+			@Override
+			public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+
+				if(!Gdx.input.isButtonPressed(Input.Buttons.LEFT))
+					return false;
+				Hexagon<TileData> data = grid.getByPixelCoordinate(screenX, screenY).get();
+				System.out.println(data.getCenterX() + " " + data.getCenterY());
+				return true;
+			}
+
+			@Override
+			public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+				return false;
+			}
+
+			@Override
+			public boolean touchDragged(int screenX, int screenY, int pointer) {
+				return false;
+			}
+
+			@Override
+			public boolean mouseMoved(int screenX, int screenY) {
+				return false;
+			}
+
+			@Override
+			public boolean scrolled(int amount) {
+				return false;
+			}
+		});
+	}
+
+	private void initHexData()
+	{
+		Observable<Hexagon<TileData>> hexagons = grid.getHexagons();
+		hexagons.forEach(hex -> {
+			hex.setSatelliteData(new TileData(hex));
+		});
+	}
+
+	private void fillHexs()
+	{
+		Observable<Hexagon<TileData>> hexagons = grid.getHexagons();
+		hexagons.forEach(hex -> {
+			TileData data = hex.getSatelliteData().get();
+    		data.setColor(0x11FF38FF);
+        });
+	}
+
+	private void renderHexs()
+	{
+		Observable<Hexagon<TileData>> hexagons = grid.getHexagons();
+		hexagons.forEach(hexagon -> {
+			Point p0 = (Point) hexagon.getPoints().toArray()[0];
+			Point pLast = (Point) hexagon.getPoints().toArray()[hexagon.getPoints().size() - 1];
+			shapeRenderer.line((float)p0.getCoordinateX(), (float)p0.getCoordinateY(),
+					(float)pLast.getCoordinateX(), (float)pLast.getCoordinateY(),
+					Color.BLACK, Color.BLACK);
+			for(int i = 1; i < hexagon.getPoints().size(); i++)
+			{
+				Point current = (Point)hexagon.getPoints().toArray()[i];
+				Point precedent = (Point)hexagon.getPoints().toArray()[i - 1];
+				shapeRenderer.line((float)current.getCoordinateX(), (float)current.getCoordinateY(),
+						(float)precedent.getCoordinateX(), (float)precedent.getCoordinateY(),
+						Color.BLACK, Color.BLACK);
+			}
+		});
+	}
 	@Override
 	public void dispose () {
 		batch.dispose();
-
 	}
 }
--- LD38/core/src/com/martenscedric/TileData.java
+++ LD38/core/src/com/martenscedric/TileData.java
@@ -1,13 +1,60 @@
 package com.martenscedric;
 
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.PolygonRegion;
+import com.badlogic.gdx.graphics.g2d.PolygonSprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.EarClippingTriangulator;
+import com.badlogic.gdx.utils.ShortArray;
+import org.codetome.hexameter.core.api.Hexagon;
+import org.codetome.hexameter.core.api.Point;
 import org.codetome.hexameter.core.api.contract.SatelliteData;
+import org.codetome.hexameter.core.api.defaults.DefaultSatelliteData;
 
 /**
  * Created by Cedric on 2017-04-21.
  */
-public class TileData implements SatelliteData
-{
+public class TileData extends DefaultSatelliteData {
     private TileType tileType;
+    private PolygonSprite sprite;
+    private Hexagon<TileData> parent;
+
+    public TileData(Hexagon<TileData> parent)
+    {
+        this.parent = parent;
+    }
+
+    public PolygonSprite getSprite() {
+        return sprite;
+    }
+
+    public void setColor(int color) {
+        Pixmap pix = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pix.setColor(color);
+        pix.fill();
+        Texture tex = new Texture(pix);
+        TextureRegion region = new TextureRegion(tex);
+
+        Point p1 = (Point) parent.getPoints().toArray()[0];
+        Point p2 = (Point) parent.getPoints().toArray()[1];
+        Point p3 = (Point) parent.getPoints().toArray()[2];
+        Point p4 = (Point) parent.getPoints().toArray()[3];
+        Point p5 = (Point) parent.getPoints().toArray()[4];
+        Point p6 = (Point) parent.getPoints().toArray()[5];
+
+        float[] vertices = new float[]{(
+                float) p1.getCoordinateX(), (float)p1.getCoordinateY(),
+                (float)p2.getCoordinateX(), (float)p2.getCoordinateY(),
+                (float)p3.getCoordinateX(), (float)p3.getCoordinateY(),
+                (float) p4.getCoordinateX(), (float)p4.getCoordinateY(),
+                (float)p5.getCoordinateX(), (float)p5.getCoordinateY(),
+                (float)p6.getCoordinateX(), (float)p6.getCoordinateY()};
+        EarClippingTriangulator triangulator = new EarClippingTriangulator();
+        ShortArray triangleIndices = triangulator.computeTriangles(vertices);
+        PolygonRegion polygonRegion = new PolygonRegion(region, vertices, triangleIndices.toArray());
+        sprite = new PolygonSprite(polygonRegion);
+    }
 
     public TileType getTileType() {
         return tileType;
@@ -15,35 +62,5 @@ public class TileData implements SatelliteData
 
     public void setTileType(TileType tileType) {
         this.tileType = tileType;
-    }
-
-    @Override
-    public boolean isPassable() {
-        return true;
-    }
-
-    @Override
-    public void setPassable(boolean passable) {
-
-    }
-
-    @Override
-    public boolean isOpaque() {
-        return true;
-    }
-
-    @Override
-    public void setOpaque(boolean opaque) {
-
-    }
-
-    @Override
-    public double getMovementCost() {
-        return 0;
-    }
-
-    @Override
-    public void setMovementCost(double movementCost) {
-
     }
 }
