beb568c26cd2a1f9963bca354f7ddaeea28b617d
MartensCedric <cedricmartens98@gmail.com>
2017-04-23 12:32:15 -0400

    Added logos and icons

--- LD38/desktop/src/com/martenscedric/desktop/DesktopLauncher.java
+++ LD38/desktop/src/com/martenscedric/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.martenscedric.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.martenscedric.LudumDare38;
@@ -11,6 +12,9 @@ public class DesktopLauncher {
 		config.height = 800;
 		config.resizable = false;
 		config.title = "HexCity";
+		config.addIcon("icons/icon-128.png", Files.FileType.Internal);
+		config.addIcon("icons/icon-32.png", Files.FileType.Internal);
+		config.addIcon("icons/icon-16.png", Files.FileType.Internal);
 		new LwjglApplication(new LudumDare38(), config);
 	}
 }
