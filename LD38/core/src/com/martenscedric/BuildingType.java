ea83b14cada4cca74385597fc9749d4730a82053
Martens Cedric <1544256@cegepmontpetit.ca>
2017-04-24 15:54:20 -0400

    Fixed tooltips

--- LD38/core/src/com/martenscedric/BuildingType.java
+++ LD38/core/src/com/martenscedric/BuildingType.java
@@ -1,19 +1,18 @@
 package com.martenscedric;
-
 /**
  * Created by Cedric on 2017-04-22.
  */
 public enum  BuildingType
 {
-    NONE("None", "You should never see this message", 0),
-    FARM("Farm", "Provides food to every adjacent tile. Cannot be placed next to another farm.", -1),
-    HOUSE("Worker house", "Provides a worker to every adjacent tile. Needs an adjacent farm.", -1),
-    MINE("Mine", "Provides minerals to every adjacent tiles. Needs an adjacent worker", 1),
-    WIND("Wind turbine", "Produces energy to every adjacent tile. Needs an adjacent worker.", 1),
-    FACTORY("Factory", "Produces consumer goods to every adjacent tile. Needs an adjacent worker, wind turbine and minerals", 2),
-    MARKET("Market", "Produces trade to every adjacent tile. Needs an adjacent worker, wind turbine and a factory", 3),
-    BANK("Bank", "Produces money to every adjacent tile. Needs an adjacent worker, market, minerals and wind turbine", 5),
-    ROCKET("Rocket", "The apex of production. Needs an adjacent worker, bank, factory and wind turbine.", 8);
+    NONE("None", "You should never see this message.", 0),
+    FARM("Farm", "Cannot be placed next to another farm.", -1),
+    HOUSE("Worker house", "Requires :\n-Farm", -1),
+    MINE("Mine", "Requires :\n-House", 1),
+    WIND("Wind turbine", "Requires :\n-House", 1),
+    FACTORY("Factory", "Requires :\n-House\n-Wind Turbine\n-Mine", 2),
+    MARKET("Market", "Requires :\n-House\n-Wind Turbine\n-Factory", 3),
+    BANK("Bank", "Requires :\n-House\n-Wind Turbine\n-Mine\n-Market", 5),
+    ROCKET("Rocket", "Requires :\n-House\n-Wind Turbine\n-Factory\n-Bank", 8);
 
     private String name;
     private String desc;
